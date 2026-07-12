120720a52b508edc2bc729224e2a342b4cd4d13f
nodirshohx9 <nodirshohx9@gmail.com>
2026-07-12 19:43:28 +0500

    Update upload.js

--- api/upload.js
+++ api/upload.js
@@ -22,8 +22,8 @@ export default async function handler(req, res) {
       }
     });
 
-    return res.status(200).json(jsonResponse);
-  } catch (error) {
+   } catch (error) {
+    console.error('Upload xatoligi:', error.message);
     return res.status(400).json({ error: error.message });
   }
 }
